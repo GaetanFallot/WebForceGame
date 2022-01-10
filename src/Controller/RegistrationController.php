999a10123975cca986856cc20020281b1f2e8cdc
Enis CIG <hiherz67@gmail.com>
2022-01-10 11:44:26 +0100

    *

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -48,7 +48,7 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('webforcegameGaetan@gmail.com', 'Staff'))
+                    ->from(new Address('eniscigtest@gmail.com', 'Staff'))
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
