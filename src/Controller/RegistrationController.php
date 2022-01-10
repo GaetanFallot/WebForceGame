c7f5aeb8c95b556fd1b934b50df76a41e7083a12
Enis CIG <hiherz67@gmail.com>
2022-01-10 11:44:26 +0100

    *

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -52,6 +52,17 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
             $this->addFlash('success', 'Inscription réussie');
 
+<<<<<<< layla
+=======
+            // generate a signed url and email it to the user
+            $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
+                (new TemplatedEmail())
+                    ->from(new Address('eniscigtest@gmail.com', 'Staff'))
+                    ->to($user->getEmail())
+                    ->subject('Please Confirm your Email')
+                    ->htmlTemplate('registration/confirmation_email.html.twig')
+            );
+>>>>>>> *
             // do anything else you need here, like send an email
             $this->mailer->sendEmail($user->getEmail(), $user->getUserToken());
 
