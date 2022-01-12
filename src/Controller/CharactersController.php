749d3f68a814c2bc1775487129cacec75341ff04
Gaëtan <gaetanfallot67@gmail.com>
2022-01-12 12:05:25 +0100

    [UP] createCharacters fonctionnel

--- src/Controller/CharactersController.php
+++ src/Controller/CharactersController.php
@@ -2,15 +2,9 @@
 
 namespace App\Controller;
 
-use App\Entity\Characters;
-use App\Form\CharactersType;
-use App\Service\FileUploader;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Validator\Constraints\Unique;
 
 class CharactersController extends AbstractController
 {
@@ -22,31 +16,6 @@ class CharactersController extends AbstractController
         ]);
     }
 
-
-    #[Route('/createCharacters', name: 'createCharacters')]
-    public function create(
-        Request $request, 
-        EntityManagerInterface $manager,
-        FileUploader $fileUploader
-        ): Response
-    {
-        $characters = new Characters();
-        $form = $this->createForm( CharactersType::class, $characters);
+    // ici faire characters liste
         
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()){
-
-            if($file = $form->get('image')->getData()){
-                $fileUploader->upload($characters, $file);
-            }
-            
-            $manager -> persist($characters);
-            $manager->flush();
-        }
-
-        return $this->render('characters/index.html.twig', [
-            'form' => $form->createView()
-        ]);
-    }
 }
---
+++ src/Controller/User/CharactersController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller\User;
+
+use App\Entity\Characters;
+use App\Form\CharactersType;
+use App\Service\FileUploader;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CharactersController extends AbstractController
+{
+
+    #[Route('/createCharacters', name: 'createCharacters')]
+    public function create(
+        Request $request, 
+        EntityManagerInterface $manager,
+        FileUploader $fileUploader
+        ): Response
+    {
+        
+        $characters = new Characters();
+        $form = $this->createForm( CharactersType::class, $characters);
+        
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+
+            if($file = $form->get('image')->getData()){
+                $fileUploader->upload($characters, $file);
+            }
+            $characters->setUser($this->getUser());
+            $manager -> persist($characters);
+            $manager->flush();
+        }
+
+        return $this->render('characters/index.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+       
+}
