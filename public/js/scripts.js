acc600e1e6fb29d87b34cb8674c6337b5b3ea096
LAYLA <layla.kali@hotmail.fr>
2022-01-12 10:56:48 +0100

    test

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -5,3 +5,26 @@ cards.forEach(card => {
         card.classList.toggle('is-flipped');
     });
   })
+
+
+  $(document).ready(function() {
+      let formData = {};
+      $("form input, form select").on("input",function(){
+        $("form input, form select").each(function(){
+            const name = $(this).attr("name").replace("characters[","").replace("]","")
+            const value = $(this).val()
+            if (name === "profession") {
+                console.log($(this).text())
+                value = ""
+            }
+            
+            
+            formData[name] = value
+        })
+        for(const name in formData){
+            const value = formData[name]
+            $("#display-character-"+ name).text(value)
+
+        }
+      });
+  })
