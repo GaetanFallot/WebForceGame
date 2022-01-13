616c6b1cec25a497b57806a8dd732ec60bc3a061
Enis CIG <hiherz67@gmail.com>
2022-01-13 15:46:05 +0100

    Admin dashboard

--- src/Controller/Admin/DashBoardController.php
+++ src/Controller/Admin/DashBoardController.php
@@ -2,17 +2,31 @@
 
 namespace App\Controller\Admin;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DashBoardController extends AbstractController
 {
-    #[Route('/a/admin', name: 'admin_dashboard')]
+    #[Route('/dashboard', name: 'admin_dashboard')]
     public function index(): Response
     {
         return $this->render('admin/dashboard/index.html.twig', [
             'controller_name' => 'DashBoardController',
         ]);
     }
+
+    #[Route('delete/user', name: 'delete_user')]
+    public function getUsersList(UserRepository $UserRepository): Response
+    {
+        $users = $UserRepository->findAll();
+        return $this->render('delete_user/index.html.twig', [
+            'users' => $users
+            ]
+        );
+
+
+    }
+
 }
